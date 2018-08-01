6dc09fd440ab52ddac13a3cc6886eac8e4aa3f2a
zzzzgc <1098766713@qq.com>
2018-08-01 18:36:58 +0800

    ceshi

--- src/pages/counter/index.vue
+++ src/pages/counter/index.vue
@@ -5,42 +5,42 @@
       <button @click="increment">+</button>
       <button @click="decrement">-</button>
     </p>
-
     <a href="/pages/index/main" class="home">去往首页</a>
   </div>
 </template>
 
 <script>
-// Use Vuex
-import store from './store'
+  // Use Vuex
+  import store from './store'
 
-export default {
-  computed: {
-    count () {
-      return store.state.count
-    }
-  },
-  methods: {
-    increment () {
-      store.commit('increment')
+  export default {
+    computed: {
+      count () {
+        return store.state.count
+      }
     },
-    decrement () {
-      store.commit('decrement')
+    methods: {
+      increment () {
+        store.commit('increment')
+      },
+      decrement () {
+        store.commit('decrement')
+      }
     }
   }
-}
 </script>
 
 <style>
-.counter-warp {
-  text-align: center;
-  margin-top: 100px;
-}
-.home {
-  display: inline-block;
-  margin: 100px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
-}
+  .counter-warp {
+    text-align: center;
+    margin-top: 100px;
+  }
+
+  .home {
+    display: inline-block;
+    margin: 100px auto;
+    padding: 5px 10px;
+    color: blue;
+    border: 1px solid blue;
+  }
 </style>
